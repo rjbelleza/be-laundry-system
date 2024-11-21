feb8ba23708e902d8f56c9f61a39dfc4747e9a8c
rjbelleza <rjbelleza19@gmail.com>
2024-11-21 23:51:02 +0800

    Updated the Migration file with new fields

--- database/migrations/2024_10_31_143925_create_personal_access_tokens_table.php
+++ database/migrations/2024_10_31_143925_create_personal_access_tokens_table.php
@@ -9,17 +9,18 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-        public function up(): void
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
+    public function up(): void 
+    { 
+        Schema::create('users', function (Blueprint $table) { 
+            $table->id(); 
+            $table->string('name'); 
+            $table->string('email')->unique(); 
+            $table->timestamp('email_verified_at')->nullable(); 
+            $table->string('password'); $table->string('address'); 
+            $table->unsignedBigInteger('mobile')->length(11); // Mobile number field 
+            $table->unsignedInteger('postal_code')->length(11); // Postal code field 
+            $table->rememberToken(); $table->timestamps(); 
+        }); 
     }
 
 
