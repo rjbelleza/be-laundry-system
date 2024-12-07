718694910f2162eacf65826909326d78816dafcc
rjbelleza <rjbelleza19@gmail.com>
2024-12-07 15:51:25 +0800

    Defined relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,4 +51,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
